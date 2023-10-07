d6292e7e66225f94427cf8ac4da005580f69f153
Aman Tyagi <91416279+AMAN-349@users.noreply.github.com>
2023-10-08 00:39:06 +0530

    Add files via upload

---
+++ Siderbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./css/Sidebar.css"
+import SidebarOptions from './SidebarOptions'
+
+function Siderbar() {
+  return (
+    <div className="sidebar">
+      <SidebarOptions/>
+    </div>
+  )
+}
+
+export default Siderbar
